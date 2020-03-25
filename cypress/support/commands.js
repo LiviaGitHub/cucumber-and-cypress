c7f0d1af103ceda13255e6f45ec703ac9b7f65a4
Livia Bonifacio <liviabonifacio@ip-192-168-178-24.eu-west-1.compute.internal>
2020-03-25 12:47:06 +0100

    cypress_project

---
+++ cypress/support/commands.js
@@ -0,0 +1,33 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add("login", (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This is will overwrite an existing command --
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+//
+// -- How testing on mobile device emulators -- 
+//    Before cy.visit() add the device or size. 
+//    cy.viewport('iphone-x')
+//    cy.viewport('ipad-mini')
+//    cy.viewport('1980, 1080')
+//
+//
